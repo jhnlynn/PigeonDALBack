a2f8be319bdfc102ce6f05992b7089cb81649f7b
jhnlynn <jl12183@nyu.edu>
2022-02-26 01:21:14 -0500

    pigeon's 1st commit

---
+++ src/main/java/pigeon/backend/pigeondalback/PigeonDalBackApplication.java
@@ -0,0 +1,13 @@
+package pigeon.backend.pigeondalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonDalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigeonDalBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pigeon/backend/pigeondalback/PigeonDalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pigeon.backend.pigeondalback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PigeonDalBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
