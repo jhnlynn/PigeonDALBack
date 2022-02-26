5d9dd7c3507cecb4becc2625c8d937032073189c
Jason <jasonguan0107@gmail.com>
2022-02-26 16:53:35 -0500

    shipment to-do

---
+++ src/main/java/pigeon/backend/pigeondalback/constants/SystemConstant.java
@@ -0,0 +1,9 @@
+package pigeon.backend.pigeondalback.constants;
+
+public class SystemConstant {
+
+    /**
+     * Shippo API
+     */
+    public static final String API_KEY = "shippo_live_960b7e9f2b759c870e9c09496001d7fb4b86e4f2";
+}
