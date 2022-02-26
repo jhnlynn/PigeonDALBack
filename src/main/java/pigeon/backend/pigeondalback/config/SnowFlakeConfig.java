9c987a79c0900f179f6ff8c09342130139c101f7
jhnlynn <jl12183@nyu.edu>
2022-02-26 10:09:21 -0500

    use snowflake id generator, and something esle

---
+++ src/main/java/pigeon/backend/pigeondalback/config/SnowFlakeConfig.java
@@ -0,0 +1,9 @@
+package pigeon.backend.pigeondalback.config;/**
+* <p>
+*
+* </p>
+* @author Johannes
+* @date 2/26/22
+*/
+public class SnowFlakeConfig {
+}
---
+++ src/main/java/pigeon/backend/pigeondalback/controller/BaseController.java
@@ -0,0 +1,9 @@
+package pigeon.backend.pigeondalback.controller;/**
+* <p>
+*
+* </p>
+* @author Johannes
+* @date 2/26/22
+*/
+public class BaseController {
+}
