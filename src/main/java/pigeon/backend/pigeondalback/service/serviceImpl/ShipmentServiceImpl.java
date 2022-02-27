a0a363f70e81324f6a7a6142d6580960f3f53f8b
jhnlynn <jl12183@nyu.edu>
2022-02-26 22:51:47 -0500

    shipment service impl

--- src/main/java/pigeon/backend/pigeondalback/service/serviceImpl/ShipmentServiceImpl.java
+++ src/main/java/pigeon/backend/pigeondalback/service/serviceImpl/ShipmentServiceImpl.java
@@ -1,4 +1,52 @@
 package pigeon.backend.pigeondalback.service.serviceImpl;
 
-public class ShipmentServiceImpl {
+import org.springframework.stereotype.Service;
+import pigeon.backend.pigeondalback.mapper.BatchMapper;
+import pigeon.backend.pigeondalback.service.ShipmentService;
+import pigeon.backend.pigeondalback.vo.DonateRequestVO;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service("shipmentService")
+public class ShipmentServiceImpl implements ShipmentService {
+
+    @Resource
+    BatchMapper batchMapper;
+
+    public List<DonateRequestVO> getHistory(String uid, Long size, Long page) throws Exception {
+        List<DonateRequestVO> donates;
+        HashMap<String, String> paramMap = new HashMap<>();
+
+        paramMap.put("uid", uid);
+        paramMap.put("size", String.valueOf(size));
+        paramMap.put("page", String.valueOf(page));
+
+        try {
+            donates = batchMapper.getDonateHistory(paramMap);
+        } catch (Exception ex) {
+            throw new Exception("Thrown By PostS, " + ex);
+        }
+
+        return donates;
+    }
+
+    public DonateRequestVO getDonationRecord(String uid, String shipId) throws Exception {
+        // getDonationRecord
+        DonateRequestVO donate;
+        HashMap<String, String> paramMap = new HashMap<>();
+
+        paramMap.put("uid", uid);
+        paramMap.put("shipId", shipId);
+
+        try {
+            donate = batchMapper.getDonationRecord(paramMap);
+        } catch (Exception ex) {
+            throw new Exception("Thrown By PostS, " + ex);
+        }
+
+        return donate;
+    }
 }
---
+++ src/main/java/pigeon/backend/pigeondalback/vo/DonateRequestVO.java
@@ -0,0 +1,37 @@
+package pigeon.backend.pigeondalback.vo;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Johannes
+ * @date 2/26/22
+ */
+public class DonateVO {
+    private String uid;
+
+    private Long bid;
+
+    private Long bnum;
+
+    private Byte bstatus;
+
+    private String btrack;
+
+    private String donationsDid;
+
+    private Byte carrierCid;
+
+    private Byte warehouseWid;
+
+    private Byte manufacturerMid;
+
+    private Byte productPid;
+
+    private String shipmentSid;
+
+    private String requestRid;
+
+    private 
+}
